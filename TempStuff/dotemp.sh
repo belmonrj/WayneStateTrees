b0c7f5749b68184d29a45f8eb9172f3791beca42
Ron Belmont <belmont@rhic21.physics.wayne.edu>
2017-06-01 12:20:18 -0400

    Added a few more files to the repo

---
+++ TempStuff/dotemp.sh
@@ -0,0 +1,65 @@
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/097/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-097.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/098/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-098.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/099/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-099.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/100/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-100.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/101/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-101.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/102/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-102.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/103/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-103.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/104/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-104.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/105/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-105.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/106/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-106.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/107/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-107.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/109/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-109.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/110/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-110.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/111/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-111.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/112/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-112.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/113/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-113.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/114/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-114.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/115/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-115.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/116/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-116.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/117/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-117.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/118/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-118.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/119/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-119.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/120/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-120.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/122/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-122.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/123/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-123.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/124/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-124.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/125/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-125.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/126/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-126.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/127/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-127.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/128/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-128.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/131/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-131.root 
+
+alien_cp alien:///alice/cern.ch/user/r/rbelmont/TaskData/TrialRunX9_LHC11h_AOD145/009/132/OutTaskData.root TrialRunX9_AOD145/WSUlocaldataX9_LHC11h_AOD145_169167-132.root 
+
